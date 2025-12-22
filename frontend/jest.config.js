0f5d58dcf8cec4557023c05e308d33a6c0096321
Finn Terdal <finnterdal@gmail.com>
2025-12-22 15:01:44 -0600

    Adds basic react test

---
+++ frontend/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/src/__mocks__/styleMock.js',
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(.*\\.mjs$))',
+  ],
+  testMatch: [
+    '**/__tests__/**/*.(js|jsx)',
+    '**/*.(test|spec).(js|jsx)',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/**/*.test.{js,jsx}',
+    '!src/index.js',
+  ],
+};
+
