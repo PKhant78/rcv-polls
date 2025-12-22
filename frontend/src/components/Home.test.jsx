c2c7625019f4e0be2252272d0fb42697ad1a67f1
Finn Terdal <finnterdal@gmail.com>
2025-12-22 15:19:43 -0600

    downgrade react for testing library

--- frontend/src/components/Home.test.jsx
+++ frontend/src/components/Home.test.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Home from './Home';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from "./Home";
 
-describe('Home', () => {
-  it('renders the expected welcome text', () => {
+describe("Home", () => {
+  it("renders the welcome text", () => {
     render(<Home />);
-    
-    expect(screen.getByText('Welcome to the TTP Winter Frontend!')).toBeInTheDocument();
-    expect(screen.getByText('Edit this file (frontend/src/components/Home.jsx) to get started!')).toBeInTheDocument();
+
+    expect(
+      screen.getByText("Welcome to the TTP Winter Frontend!")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        "Edit this file (frontend/src/components/Home.jsx) to get started!"
+      )
+    ).toBeInTheDocument();
   });
 });
-
